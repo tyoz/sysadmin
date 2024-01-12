71e1cd5e507f72ef9c8b477e994a13225e5755bf
Tao <tao.zanella@gmail.com>
2024-01-12 11:21:32 -0300

    Added permission matrix.

--- manage.sh
+++ manage.sh
@@ -7,3 +7,14 @@ setfacl -m u:username:rwx folder_name
 setfacl is short for set File ACL (Access Control List)
 
 setfacl -R -m u:username:rwx folder_name #recursive
+
+
+# permission matrix
+# 7 = all rights
+# 6 = read and write
+# 5 = read and execute
+# 4 = read only
+# 3 = execute and write
+# 2 = write only
+# 1 = execute only
+# 0 = no rights
